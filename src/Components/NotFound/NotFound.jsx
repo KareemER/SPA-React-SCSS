f719d28ec312b3c18a64d65be9c49bf4d4cfcf8f
KareemER <93328819+KareemER@users.noreply.github.com>
2025-01-08 23:38:27 +0200

    Update NotFound.jsx
    for better UI

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 export default function NotFound() {
   return (
     <>
+    <section className="notfound">
       <div className="container d-flex flex-column justify-content-center align-items-center">
       <FontAwesomeIcon icon="fa-sharp fa-solid fa-circle-exclamation" bounce size="2xl" style={{color: "#74C0FC",}} />
-      <h2 className="text-center">not-found works!</h2>
+      <h2>not-found works!</h2>
       </div>
+    </section>
     </>
   )
 }
